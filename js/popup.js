87fc077351d58b9660b47d2485c931aa670513a7
Ryohei Nihiyama <ryouhei222@gmail.com>
2016-09-10 16:35:51 +0900

    apiの利用メソッドを変更　

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,9 @@
 $(function () {
-	chrome.tabs.getSelected(null, function(tab) {
+	chrome.tabs.query({
+		active: true,
+		lastFocusedWindow: true
+	}, function(tabs) {
+		var tab = tabs[0];
 		//$('#key-val').data('issue-key')
 		$('#title').text(tab.title);
 		//$('#url').text(tab.url);
